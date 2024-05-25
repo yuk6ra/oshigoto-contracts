097c81661e6411e4e148895ffaae94103f6f1c25
yuk6ra <y@imamu.org>
2024-05-25 17:57:05 +0900

    Update ERC20 Payment

--- src/OshigotoToken.sol
+++ src/OshigotoToken.sol
@@ -5,6 +5,7 @@ import "./dn404/DN404.sol";
 import "./dn404/DN404Mirror.sol";
 import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 import {Strings} from "openzeppelin-contracts/contracts/utils/Strings.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 /**
  * @title SimpleDN404
@@ -19,6 +20,11 @@ contract OshigotoToken is DN404, Ownable {
 
     string public baseTokenURI = "https://example.com/token/";
     string public dataURI = "https://example.com/data/";
+
+    uint256 public priceWithERC20Token = 2000;
+    uint256 public priceWithNativeToken = 100_000_000_000_000_000;
+    uint256 public mintAmount = 1;
+    IERC20 public paymentToken;
 
     constructor(
         string memory name_,
@@ -89,16 +95,60 @@ contract OshigotoToken is DN404, Ownable {
             }
     }
 
-    // This allows the owner of the contract to mint more tokens.
-    function mint(address to, uint256 amount) public {
-        _mint(to, amount);
+    function mintWithERC20Token() public {
+        require(paymentToken.balanceOf(msg.sender) >= priceWithERC20Token, "Insufficient token balance");
+        require(paymentToken.allowance(msg.sender, address(this)) >= priceWithERC20Token, "Token allowance too low");
+
+        paymentToken.transferFrom(msg.sender, address(this), priceWithERC20Token);
+
+        _mint(msg.sender, mintAmount);
+    }
+
+    function mintWithNativeToken() public payable {
+        require(msg.value >= priceWithNativeToken, "Insufficient payment");
+        
+        payable(owner()).transfer(msg.value);
+
+        _mint(msg.sender, mintAmount);
+    }
+
+    function ownerMint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
+
+    function airdrop(address[] calldata _recipients, uint256[] calldata _amounts) public onlyOwner {
+        require(_recipients.length == _amounts.length, "Array lengths must match");
+        for (uint256 i = 0; i < _recipients.length; i++) {
+            _mint(_recipients[i], _amounts[i]);
+        }
     }
 
     function setBaseURI(string calldata baseURI_) public onlyOwner {
         _baseURI = baseURI_;
     }
 
-    function withdraw() public onlyOwner {
+    function setMintAmount(uint256 _amount) public onlyOwner {
+        mintAmount = _amount;
+    }
+
+    function setPaymentTokenAddress(address _address) public onlyOwner {
+        paymentToken = IERC20(_address);
+    }
+
+    function setPriceWithERC20Token(uint256 price) public onlyOwner {
+        priceWithERC20Token = price;
+    }
+
+    function setPriceWithNativeToken(uint256 price) public onlyOwner {
+        priceWithNativeToken = price;
+    }
+
+    function withdrawWithNativeToken() public onlyOwner {
         payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function withdrawWithERC20Token(address tokenAddress) public onlyOwner {
+        IERC20 token = IERC20(tokenAddress);
+        token.transfer(msg.sender, token.balanceOf(address(this)));
     }
 }
--- test/OshigotoToken.t.sol
+++ test/OshigotoToken.t.sol
@@ -3,20 +3,47 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import {OshigotoToken} from "../src/OshigotoToken.sol";
+import {LoginCoin} from "../src/LoginCoin.sol";
 
 contract OshigotoTest is Test {
     OshigotoToken dn;
     address alice = address(111);
 
+    LoginCoin loginCoin;
+
     function setUp() public {
         vm.prank(alice);
-        dn = new OshigotoToken("DN404", "DN", 1000, address(this));
+        dn = new OshigotoToken("DN404", "DN", 0, address(this));
+
+        loginCoin = new LoginCoin("LoginCoin", "LC");
     }
 
     function testMint() public {
+
         vm.prank(dn.owner());
-        dn.mint(alice, 100);
-        assertEq(dn.totalSupply(), 1100);
+        dn.setPriceWithNativeToken(1);
+        dn.mintWithNativeToken{value: 1}();
+
+        assertEq(dn.totalSupply(), 1);
+    }
+
+    function testMintWithERC20Token() public {
+        vm.startPrank(alice);
+
+        loginCoin.mint(alice, 200);
+        console.log("alice balance: ", loginCoin.balanceOf(alice));
+        loginCoin.approve(address(dn), 100);
+
+        dn.setPaymentTokenAddress(address(loginCoin));
+        dn.setPriceWithERC20Token(100);
+
+        dn.mintWithERC20Token();
+        console.log("alice balance: ", loginCoin.balanceOf(alice));
+        console.log("OshigotoToken balance: ", dn.balanceOf(alice));
+
+        assertEq(dn.totalSupply(), 1);
+
+        vm.stopPrank();
     }
 
     function testName() public view {
@@ -31,7 +58,7 @@ contract OshigotoTest is Test {
         vm.deal(address(dn), 1 ether);
         assertEq(address(dn).balance, 1 ether);
         vm.prank(alice);
-        dn.withdraw();
+        dn.withdrawWithNativeToken();
         assertEq(address(dn).balance, 0);
         assertEq(alice.balance, 1 ether);
     }
