67927a3e1764704c9437dfc4b8282b286799c1c5
yuk6ra <y@imamu.org>
2024-05-25 17:56:42 +0900

    Add LoginCoin contract for ERC20 token minting

---
+++ script/mocks/DeployLoginCoin.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import {LoginCoin} from "../../src/LoginCoin.sol";
+import "forge-std/Script.sol";
+
+contract LoginCoinScript is Script {
+    uint256 private constant _WAD = 1000000000000000000;
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("DEV_PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        // SimpleDN404 constructor args -- name, symbol, initialSupply, owner
+        // CHANGE THESE VALUES TO SUIT YOUR NEEDS
+        string memory name = "LoginCoin";
+        string memory symbol = "LC";
+        address owner = address(this);
+
+        new LoginCoin(name, symbol);
+        vm.stopBroadcast();
+    }
+}
